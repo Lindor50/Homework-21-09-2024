86cebe2918285c33fc28e71616c612c2d9d34573
Lindor50 <dbsdbs2@inbox.ru>
2024-09-21 23:35:50 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+name = "Daniel"
+print ("Hello" + name)
+
+
+name = "Daniel"
+print(name[0:3])
+print(name[0:3:2])
+
+example = "Смерть"
+print (example[0])
+print (example[-1])
+print (example[2:6])
+print (example[::-1])
+print (example[1:6:2])
