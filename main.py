cdffaa73f8533912f2ecdc30bfcda2f2281a7c09
Lindor50 <dbsdbs2@inbox.ru>
2024-09-23 02:20:21 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -11,4 +11,19 @@ print (example[0])
 print (example[-1])
 print (example[2:6])
 print (example[::-1])
-print (example[1:6:2])
+print (example[0:6:2])
+
+name = "Daniel"
+print(name)
+name = "Lindor"
+print(name)
+
+date_of_birth = "March 2003"
+dateOfBirth = "March 2003"
+
+
+Number_of_homework = 12
+Number_of_hours = 1.5
+Course_name = 'Python'
+Time_for_one_homework = Number_of_hours / Number_of_homework
+print(Course_name,', всего задач:',Number_of_homework,', затрачено часов:',Number_of_hours,', среднее время выполнения',Time_for_one_homework, 'часа')
